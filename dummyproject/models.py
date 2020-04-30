d893f3386d23d0e952ad00d0452c7ba88485886b
Aleksandar Staykov <astaykov@localhost.localdomain>
2020-04-30 12:59:54 +0200

    extended datamodel

--- dummyproject/models.py
+++ dummyproject/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=50, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
     age = models.IntegerField(null=True, blank=True)
     cool = models.BooleanField(null=True, blank=True)
+    date_of_coolness = models.DateTimeField(null=True, blank=True)
+    amount_of_coolness = models.DecimalField(decimal_places=2,max_digits=5,null=True, blank=True)
+    
